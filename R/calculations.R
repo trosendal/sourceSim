f0ee425e2dc7a525cecf41d81c7e326ad2d4178a
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2021-02-25 16:28:53 +0100

    Add some parameters

--- R/calculations.R
+++ R/calculations.R
@@ -1,10 +1,15 @@
+
+## Nucleotide frequencies
 sequences <-
-    sapply(list.files("data/MLST_alleles", full.names = T), readLines)
+    sapply(list.files("data/MLST_alleles", full.names = T),
+           function(x) {
+               seq <- readLines(x)
+               paste(seq[!grepl("^>", seq)], collapse = "")
+           })
 
 combined <- unlist(sequences)
 
-bases <- paste(combined[!grepl("^>", combined)],
-               collapse = "")
+bases <- paste(sequences, collapse = "")
 
 n_bases <- nchar(bases)
 
@@ -13,6 +18,22 @@ frequencies <- sapply(c("A", "T", "C", "G"),
                           lengths(regmatches(bases,
                                              gregexpr(x, bases)))) / n_bases
 
-frequencies <- paste0(names(frequencies), ": ", frequencies)
+params <- paste0("PRO", names(frequencies), ":\t", frequencies)
 
-writeLines(frequencies, con = "data/nucleotide_frequencies.txt")
+## Mutation rate for C. jejuni
+## Estimates from Wilson et al. (2009, Mol Biol Evol)
+## https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2639114/
+generation_length <- 2.79e-4    # years
+mutation_rate <- 3.23e-2 *  # per kilobase per year
+    generation_length * 1000    # per base per generation
+
+params <- c(params, paste0("MUTR:\t", mutation_rate))
+
+## Recombination rate relative to mutations
+## Estimate from Yu et al. (2012, J Mol Evol)
+## https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3985069/
+rr <- 6.96
+
+params <- c(params, paste0("RECR:\t", rr))
+
+writeLines(params, con = "data/params.txt")
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+## Source attribution simulation
 The idea is to use the methods described in this paper:
 https://doi.org/10.1093/bioinformatics/bty093 and its accompanying
 software: https://bitbucket.org/aleksisipola/bacmeta/src/master/ to
@@ -6,3 +7,11 @@ overlapping sources and a human population to create simulated dataset
 to perform source attribution on. This will be used to test the
 efficacy of the existing methods of source attribution for at least
 the microbial subtyping methods.
+
+## Parameters:
+
+- Nucleotide frequencies
+    - A: 0.322069733492811
+    - T: 0.314661475155537
+    - C: 0.149941288086396
+    - G: 0.213327503265256
