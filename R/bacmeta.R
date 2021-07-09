fe7bf50cbe99fa8d4bd3df07138ad3008575208f
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2021-07-09 14:00:49 +0200

    Small code reshuffles

--- R/bacmeta.R
+++ R/bacmeta.R
@@ -17,21 +17,19 @@ bacmeta_binaries <- function()
             "Superpop.o")
       ))
 
-
 ##' Check if Bacmeta is Compiled
 ##'
 ##' If bacmeta is compiled, each C++ script in its src directory should have
 ##' a corresponding .o file, and there should be a "Simu" file in bacmeta root.
 ##'
-##' @return \code{TRUE} if bacmeta is compiled, otherwise \code{FALSE}
-##' @author Wiktor Gustafsson
+##' @return \code{TRUE} if bacmeta is compiled, otherwise \code{FALSE}.
 is_bacmeta_compiled <- function() {
-    all(file.exists(file.path(path_to_bacmeta(), bacmeta_binaries())))
+    all(file.exists(file.path(
+        path_to_bacmeta(), bacmeta_binaries()
+    )))
 }
 
 ##' Compile bacmeta
-##'
-##' @author Wiktor Gustafsson
 ##' @export
 compile_bacmeta <- function() {
     cat("Compiling bacmeta...\n")
@@ -39,8 +37,8 @@ compile_bacmeta <- function() {
         cat("Bacmeta already compiled.\n")
     } else {
         wd <- setwd(file.path(path_to_bacmeta(), "src"))
+        on.exit(setwd(wd))
         system("make")
-        setwd(wd)
         cat ("Compilation successful.\n")
     }
 }
