e47c236cdc848950d7efc1a451b22e139827981f
Thomas Rosendal <thomas.rosendal@sva.se>
2021-09-08 09:29:02 +0200

    Change compilation methods to on the fly

--- Makefile
+++ Makefile
@@ -5,15 +5,11 @@ PKG_NAME=$(shell grep -i ^package DESCRIPTION | cut -d : -d \  -f 2)
 # Name of built package
 PKG_TAR=$(PKG_NAME)_$(PKG_VERSION).tar.gz
 
-all: bacmeta
+all: install
 
 # Install package
 install:
 	cd .. && R CMD INSTALL $(PKG_NAME)
-
-# compile bacmeta
-bacmeta: install
-	Rscript -e "sourceSim::compile_bacmeta()"
 
 # Build documentation with roxygen
 # 1) Remove old doc
@@ -34,4 +30,4 @@ check:
 	cd .. && _R_CHECK_CRAN_INCOMING_=FALSE R CMD check \
         --no-stop-on-test-error --as-cran --run-dontrun $(PKG_TAR)
 
-.PHONY: install bacmeta roxygen pdf check all
+.PHONY: install roxygen pdf check all
--- R/bacmeta.R
+++ R/bacmeta.R
@@ -1,7 +1,7 @@
 ##' Path to bacmeta
 ##' @noRd
 path_to_bacmeta <- function()
-    system.file("bacmeta", package = "sourceSim")
+    bacmeta_path_object()$bacmeta
 
 ##' These files exist if bacmeta is compiled
 ##' @noRd
@@ -24,9 +24,10 @@ bacmeta_binaries <- function()
 ##'
 ##' @return \code{TRUE} if bacmeta is compiled, otherwise \code{FALSE}.
 is_bacmeta_compiled <- function() {
-    all(file.exists(file.path(
-        path_to_bacmeta(), bacmeta_binaries()
-    )))
+    all(!is.null(path_to_bacmeta()),
+        file.exists(file.path(
+            path_to_bacmeta(), bacmeta_binaries()
+        )))
 }
 
 ##' Compile bacmeta
@@ -36,8 +37,13 @@ compile_bacmeta <- function() {
     if (is_bacmeta_compiled()) {
         cat("Bacmeta already compiled.\n")
     } else {
-        wd <- setwd(file.path(path_to_bacmeta(), "src"))
+        assign("bacmeta", tempdir(), envir = bacmeta_path_object())
         on.exit(setwd(wd))
+        file.copy(from = system.file("bacmeta/src",
+                                     package = "sourceSim"),
+                  recursive = TRUE,
+                  to = ".")
+        wd <- setwd(file.path(bacmeta_path_object()$bacmeta, "src"))
         system("make")
         cat ("Compilation successful.\n")
     }
@@ -48,3 +54,14 @@ compile_bacmeta <- function() {
 ##' @export
 clean_bacmeta <- function()
     unlink(file.path(path_to_bacmeta(), bacmeta_binaries()))
+
+
+## Create a package environment to store path to compiled bacmeta
+.sourceSim_env <- new.env()
+
+##' Return the bactmeta environment
+##'
+##' @return The evironment that contains the path to compiled bactmeta
+##' @export
+bacmeta_path_object <- function()
+  .sourceSim_env
