8705ee2dd01bb449e6a6677b6ba42511010b91eb
Thomas Rosendal <thomas.rosendal@sva.se>
2022-02-07 22:14:27 +0100

    draft of testing isource

--- R/isource.R
+++ R/isource.R
@@ -42,3 +42,123 @@ compile_isource <- function(quiet = FALSE) {
         if (isFALSE(quiet)) cat("Compilation successful.\n")
     }
 }
+
+##' isource
+##'
+##' Runs isource asymmetric island model:
+##'
+##' @export
+##' @param x a sourceSim_result object
+##' @param iter the number of iterations
+##' @param burnin the burnin
+##' @param thinning the thinning rate
+##' @param dirichlet_param the parameter to the dirichlet
+##' @param group_var the name of the grouping variable
+isource <- function(x = NULL,
+                    iter = 20000,
+                    burnin = 1000,
+                    thinning = 50,
+                    dirichlet_param = 1,
+                    group_var = 'group') {
+
+    stopifnot("sourceSim_result" %in% class(x))
+    stopifnot("Pop_human" %in% names(x$population))
+
+    pops <- seq_len(x$parameters$NPOP) - 1
+    pops <- c("Pop_human", paste0("Pop_", pops))
+    npop <- length(pops) - 1
+    ## Expand to long form
+    df <- do.call("rbind", lapply(pops, function(y) {
+        df <- data.frame(pop = y,
+                         count = x$population[, y],
+                         MLST = x$population[, "MLST"],
+                         ST = x$population[, "seqID"])
+        do.call("rbind", apply(df, 1, function(z) {
+            matrix(c(rep(z[1], z[2]),
+                     rep(z[3], z[2]),
+                     rep(z[4], z[2])), ncol = 3)
+        }))
+    }))
+
+    df <- cbind(df[, c(1, 3)], do.call("rbind", strsplit(df[, 2], "-")))
+    colnames(df) <- c("group", "ST", "ASP", "GLN", "GLT", "GLY", "PGM", "TKT", "UNC")
+    df[, "group"] <- as.character(factor(df[, "group"],
+                                         levels = pops,
+                                         labels = c(0, seq_len(x$parameters$NPOP))))
+
+    df <- as.data.frame(df)
+    df <- df[, c(2, 3, 4, 5, 6, 7, 8, 9, 1)]
+
+    isource_dir <- tempdir()
+    wd <- setwd(isource_dir)
+    on.exit(setwd(wd))
+
+    write.table(x, file = "input.txt", quote = FALSE, row.names = FALSE, sep = "\t")
+
+    compile_isource()
+
+    system2(path_to_isource("isource/isource"), args = list("input.txt",
+                                                            "output.txt",
+                                                            iter,
+                                                            thinning,
+                                                            dirichlet_param,
+                                                            shQuote(group_var)))
+    mcmc <- read.table("output.txt", header = T, comment.char = "")
+    fmcmc <- read.table("output.txt", header = T, comment.char = "")
+    g <- t(matrix(scan("g_output.txt", what = double(0), sep = "\t"), nrow = npop))
+    sim <- list(mcmc = mcmc, fmcmc = fmcmc, g = g, ng = npop)
+
+    ## Set the burnin
+    gd <- sim$mcmc$iter >= burnin
+    fd <- sim$fmcmc$iter >= burnin / 2
+    df <- sim$fmcmc[fd, 2:(sim$ng+1)]
+    names(df) <- pops[-1]
+    pe <- apply(df, 2, function(x) {
+        c("mean" = mean(x),
+          "median" = median(x),
+          "sd" = sd(x),
+          quantile(x, c(.025,.975)))
+    })
+    pe
+}
+
+##' sample_humans
+##'
+##' Sample the human cases from the bacterial sources given an
+##' attribution fraction.
+##'
+##' @param x A result of a simu() function
+##' @param attribution the attribution fraction for each of the source populations
+##' @param n The number of human cases
+##' @export
+##' @return A sourceSim_result object with added humans
+sample_humans <- function(x,
+                          attribution = 1,
+                          n = 100) {
+    stopifnot("sourceSim_result" %in% class(x))
+    df <- x$population
+
+    if (length(attribution) == 1)
+        attribution <- rep(attribution, x$parameters$NPOP)
+    stopifnot(length(attribution) == x$parameters$NPOP)
+
+    pops <- seq_len(x$parameters$NPOP)
+
+    ## Given the attribution, how many cases should come from each population?
+    n_pop<- table(factor(sample(x = pops,
+                                size = n,
+                                prob = attribution,
+                                replace = TRUE),
+                         levels = pops))
+
+    ## Sample an MLST type for each human cases:
+    seqs <- unlist(lapply(seq_len(x$parameters$NPOP), function(x) {
+        sample(df[, "seqID"],
+               n_pop[x],
+               replace = TRUE,
+               prob = df[, x])
+    }))
+    df$Pop_human <- as.numeric(table(factor(seqs, levels = df[, "seqID"])))
+    x$population <- df
+    x
+}
