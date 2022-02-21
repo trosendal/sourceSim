9e2dcf832509f9e516da438ddb461f92fb9556af
Thomas Rosendal <thomas.rosendal@sva.se>
2022-02-21 11:43:21 +0100

    missed file

---
+++ R/dist.R
@@ -0,0 +1,18 @@
+##' rdirichlet
+##'
+##' sample from a dirichlet distribution. Returns a matrix where each
+##' row is a sample. The definition of a dirichlet is that each set of
+##' n samples must sum to 1. Therefore the rowSums(rdirichlet()) is
+##' always 1.
+##'
+##' @param n The number of samples from the dirichlet
+##' @param alpha The shape of each gamma distribution. The length
+##'     dictates the number of bins in the dirichlet.
+##' @export
+##' @return A matrix
+rdirichlet <- function(n = 1, alpha = c(1, 1)) {
+    g <- do.call("cbind", lapply(alpha, function(x) {
+        rgamma(n, shape = x)
+    }))
+    g / matrix(rowSums(g), nrow = n, ncol = length(alpha))
+}
