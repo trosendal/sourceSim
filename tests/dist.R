15dfb696463fe603702e0b7944b9141f89bfc9b9
Thomas Rosendal <thomas.rosendal@sva.se>
2022-02-21 11:48:08 +0100

    add test

---
+++ tests/dist.R
@@ -0,0 +1,4 @@
+library(sourceSim)
+## check that the rosum is always very close to 1
+stopifnot(all(round(rowSums(rdirichlet(100, c(10,1,1))), 5) == 1))
+stopifnot(all(round(rowSums(rdirichlet(100, c(1,1,1,1,1))), 5) == 1))
