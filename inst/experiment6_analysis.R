27757d4c561d5f80edab19adffe0e4f369ad344f
Thomas Rosendal <thomas.rosendal@sva.se>
2022-11-15 09:03:19 +0100

    added plots for large migration overlap versus error

--- inst/experiment6_analysis.R
+++ inst/experiment6_analysis.R
@@ -27,7 +27,7 @@ df$err1 <- abs(df$ob1 - df$ex1)
 df$err2 <- abs(df$ob2 - df$ex2)
 df$migtot <- df$mig01 + df$mig10
 
-## Looks like the small migration rates result in lower squared error
+## The relationship between migration and error
 cairo_pdf("plots/large_mig_error.pdf", width = 12, height = 4)
 par(mfrow = c(1, 3))
 boxplot(df$err0 ~ cut(df$migtot, 40), xlab = "sum migration rate (A\u2194B)", ylab = "Error of attribution (A)", ylim = c(0,0.50))
@@ -35,6 +35,7 @@ boxplot(df$err1 ~ cut(df$migtot, 40), xlab = "sum migration rate (A\u2194B)", yl
 boxplot(df$err2 ~ cut(df$migtot, 40), xlab = "sum migration rate (A\u2194B)", ylab = "Error of attribution (C)", ylim = c(0,0.50))
 dev.off()
 
+## The relationship between migration and variance of error (Remember variance is on the squared scale when compared to error)
 cairo_pdf("plots/large_mig_variance.pdf", width = 12, height = 4)
 par(mfrow = c(1, 3))
 plot(tapply(df$err0, cut(df$migtot, 40), var), xaxt="n", xlab = "sum migration rate (A\u2194B)", ylab = "Variance of error (A)", ylim = c(0, 0.05))
@@ -43,4 +44,30 @@ plot(tapply(df$err1, cut(df$migtot, 40), var), xaxt="n", xlab = "sum migration r
 axis(side = 1, at = 1:40, labels = levels(cut(df$migtot, 40)))
 plot(tapply(df$err2, cut(df$migtot, 40), var), xaxt="n", xlab = "sum migration rate (A\u2194B)", ylab = "Variance of error (C)", ylim = c(0, 0.05))
 axis(side = 1, at = 1:40, labels = levels(cut(df$migtot, 40)))
+dev.off()
+
+## Relationship between overlap and error
+## OBS: The first 2 bins only have 1 observation
+table(cut(df$overlap, 20))
+cairo_pdf("plots/large_mig_error_vs_overlap.pdf", width = 12, height = 4)
+par(mfrow = c(1, 3))
+boxplot(df$err0 ~ cut(df$overlap, 20), xlab = "relative overlap (A\u2194B)", ylab = "Error of attribution (A)", ylim = c(0,0.50))
+boxplot(df$err1 ~ cut(df$overlap, 20), xlab = "relative overlap (A\u2194B)", ylab = "Error of attribution (B)", ylim = c(0,0.50))
+boxplot(df$err2 ~ cut(df$overlap, 20), xlab = "relative overlap (A\u2194B)", ylab = "Error of attribution (C)", ylim = c(0,0.50))
+dev.off()
+
+## The relationship between migration and variance of error (Remember variance is on the squared scale when compared to error)
+cairo_pdf("plots/large_mig_variance_vs_overlap.pdf", width = 12, height = 4)
+par(mfrow = c(1, 3))
+plot(tapply(df$err0, cut(df$overlap, 20), var), xaxt="n", xlab = "relative overlap (A\u2194B)", ylab = "Variance of error (A)", ylim = c(0, 0.05))
+axis(side = 1, at = 1:20, labels = levels(cut(df$overlap, 20)))
+plot(tapply(df$err1, cut(df$overlap, 20), var), xaxt="n", xlab = "relative overlap (A\u2194B)", ylab = "Variance of error (B)", ylim = c(0, 0.05))
+axis(side = 1, at = 1:20, labels = levels(cut(df$overlap, 20)))
+plot(tapply(df$err2, cut(df$overlap, 20), var), xaxt="n", xlab = "relative overlap (A\u2194B)", ylab = "Variance of error (C)", ylim = c(0, 0.05))
+axis(side = 1, at = 1:20, labels = levels(cut(df$overlap, 20)))
+dev.off()
+
+## Migration versus overlap
+cairo_pdf("plots/large_migration_vs_overlap.pdf")
+boxplot(df$overlap ~ cut(df$migtot, 40), xlab = "sum migration rate (A\u2194B)", ylab = "relative overlap (A\u2194B)")
 dev.off()
