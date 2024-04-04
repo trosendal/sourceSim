3f1be22bd0e7a07cf554731fd4f6dcd0c41d11aa
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2024-04-04 13:43:03 +0200

    Experiment 3 analysis script

---
+++ inst/results/analyze_results_3.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+
+experiment <- "experiment3"
+
+experiment_dir <- file.path("inst/results", experiment)
+
+stopifnot(dir.exists(experiment_dir))
+
+result_files <- list.files(experiment_dir, full.names = TRUE)
+result_files <- result_files[tools::file_ext(result_files) == "Rds"]
+
+stopifnot(length(result_files) > 0)
+
+results <- lapply(result_files, readRDS)
+
+res_df <- do.call("rbind", lapply(results, function(x) {
+    df <- data.frame(
+        mig = x$migration,
+        overlap = x$overlap
+    )
+
+    rownames(df) <- NULL
+
+    df
+}))
+
+
+res_df$migration <- cut(res_df$mig, 20, dig.lab = 3)
+migration_bins <- levels(res_df$migration)
+
+p <- ggplot(res_df, aes(x = migration, y = overlap, group = migration)) +
+    geom_boxplot(fill = "gray") +
+    ggtitle("Migration vs overlap") +
+    scale_x_discrete(
+        limits = unique(migration_bins),
+        breaks = migration_bins[seq(1, length(migration_bins), by = 6)]
+    ) +
+    theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+
+ggsave(
+    file.path(experiment_dir, paste0(experiment, "_boxplot.pdf")), p, width = 10
+)
