3e399f7b46ac03cf0af629576daeae71e9a495e9
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2024-04-04 09:21:31 +0200

    Experiment0 boxplot

--- inst/results/analyze_results_0.R
+++ inst/results/analyze_results_0.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 attr_err <- function(ob, ex) {
     abs(ob - ex)
 }
@@ -19,7 +21,7 @@ res_df <- do.call("rbind", lapply(results, function(x) {
     attr_island <- data.frame(
         pop = colnames(x$attribution_island),
         model = "island",
-        migration = unique(as.numeric(x$migration[x$migration > 0])),
+        mig = unique(as.numeric(x$migration[x$migration > 0])),
         attr_exp = as.numeric(x$sampling),
         attr_ob = x$attribution_island[1, ]
     )
@@ -27,7 +29,7 @@ res_df <- do.call("rbind", lapply(results, function(x) {
     attr_hald <- data.frame(
         pop = colnames(x$attribution_hald),
         model = "hald",
-        migration = unique(as.numeric(x$migration[x$migration > 0])),
+        mig = unique(as.numeric(x$migration[x$migration > 0])),
         attr_exp = as.numeric(x$sampling),
         attr_ob = x$attribution_hald[1, ]
     )
@@ -43,12 +45,17 @@ res_df$error <- mapply(
     attr_err, ob = res_df$attr_ob, ex = res_df$attr_exp
 )
 
-res_df$migration <- cut(res_df$migration, 20)
-
-library(ggplot2)
+res_df$migration <- cut(res_df$mig, 20, dig.lab = 3)
+migration_bins <- levels(res_df$migration)
 
 p <- ggplot(res_df, aes(x = migration, y = error, group = migration)) +
     geom_boxplot(fill = "gray") +
-    facet_grid(model ~ pop)
+    facet_grid(model ~ pop) +
+    ggtitle("Migration range: (0, 1)") +
+    scale_x_discrete(
+        limits = unique(migration_bins),
+        breaks = migration_bins[seq(1, length(migration_bins), by = 6)]
+    ) +
+    theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
 
-ggsave(file.path(experiment_dir, "boxplot.png"), p, width = 15)
+ggsave(file.path(experiment_dir, "experiment0_boxplot.pdf"), p, width = 15)
