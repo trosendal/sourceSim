886ec78b26f1c592ec1aac8415f14bebfa577430
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2024-04-15 08:49:44 +0200

    Add function to calculate RMSE

--- R/stats.R
+++ R/stats.R
@@ -16,3 +16,25 @@ rdirichlet <- function(n = 1, alpha = c(1, 1)) {
     }))
     g / matrix(rowSums(g), nrow = n, ncol = length(alpha))
 }
+
+##' rmse
+##'
+##' Calculate the root mean square error of a series of observations to an
+##' expected value. The formula is that of standard deviation, but variation
+##' is measured around an expected (given) value rather than the mean of the
+##' data.
+##'
+##' @param ob a numeric vector of observations
+##' @param exp a numeric expected value. Must be of length 1.
+##' @return a numeric value
+##' @export
+rmse <- function(ob, exp) {
+    stopifnot(
+        is.numeric(ob),
+        is.numeric(exp),
+        length(ob) > 0,
+        length(exp) == 1
+    )
+
+    sqrt(sum((ob - exp)^2) / length(ob))
+}
