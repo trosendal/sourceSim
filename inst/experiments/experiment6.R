2c03608599a7226fb8b5740645140292837d9772
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2024-04-16 13:25:05 +0200

    Use RMSE in experiment 6

--- inst/experiments/experiment6.R
+++ inst/experiments/experiment6.R
@@ -1,13 +1,18 @@
 ## Experiment 6: Calculate the expected error given a random model output,
 ## i.e. the "expected" are sampled the same way as the observed attribution
-## frequencies. This value is the maximum error that a model needs to 
+## frequencies. This value is the maximum error that a model needs to
 ## outperform.
 
 library(sourceSim)
 
-errors <- sapply(1:10000, function(i) {
-    frequencies <- rdirichlet(2, c(1, 1, 1))
-    apply(frequencies, 2, function(x) abs(diff(x)))
+expected <-  rdirichlet(1000, c(1, 1, 1))
+
+errors <- apply(expected, 1, function(x) {
+    observed <- rdirichlet(1000, c(1,1,1))
+    sapply(seq_len(ncol(observed)), function(i) {
+        rmse(observed[, i], x[i])
+    })
 })
 
+apply(errors, 1, mean)
 mean(errors)
